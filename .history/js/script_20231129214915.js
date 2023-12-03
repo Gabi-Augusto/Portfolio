9787668336ba11f445f76e19e7156d515d968dbc
Gabriela Rodrigues Augusto <garoau20@gmail.com>
2023-12-03 20:06:53 -0300

    Update 1

---
+++ .history/js/script_20231129214915.js
@@ -0,0 +1,53 @@
+/*========== menu icon navbar ==========*/
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+
+/*========== scroll sections active link ==========*/
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a [href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+    /*========== sticky navbar ==========*/
+    let header = document.querySelector('.header');
+
+    header.classList.toggle('sticky', window.scrollY > 100); 
+    /*========== remove menu icon navbar when click navbar link (scroll) ==========*/
+};
+
+
+/*========== swiper ==========*/
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 50,
+    loop: true,
+    grabCursor: true, 
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+
+/*========== dark light mode ==========*/
+
+/*========== scroll reveal ==========*/
