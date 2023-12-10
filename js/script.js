c16b6dcc54d0f11af48399abf1a602d41b19791c
Gabriela Rodrigues Augusto <garoau20@gmail.com>
2023-12-10 16:30:04 -0300

    update

--- js/script.js
+++ js/script.js
@@ -2,6 +2,10 @@
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle('bx-x');
+  navbar.classList.toggle('active');
+}
 
 /*========== scroll sections active link ==========*/
 let sections = document.querySelectorAll('section');
@@ -26,6 +30,10 @@ window.onscroll = () => {
 
     header.classList.toggle('sticky', window.scrollY > 100); 
     /*========== remove menu icon navbar when click navbar link (scroll) ==========*/
+    
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+
 };
 
 
@@ -49,4 +57,23 @@ var swiper = new Swiper(".mySwiper", {
 
 /*========== dark light mode ==========*/
 
+let darkModeIcon = document.querySelector('#darkMode-icon');
+
+darkModeIcon.onclick = () => {
+  darkModeIcon.classList.toggle('bx-sun');
+  document.body.classList.toggle('dark-mode');
+}
+
 /*========== scroll reveal ==========*/
+
+ScrollReveal({
+   reset: true,
+   distance: '80px',
+   duration: 2000,
+   delay: 200
+  });
+
+  ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+  ScrollReveal().reveal('.home-img img, .services-container, .portfolio-box, .testimonial-wrapper, .contact form', { origin: 'bottom' });
+  ScrollReveal().reveal('.home-content h1, .about-img img', { origin: 'left' });
+  ScrollReveal().reveal('.home-content h3, .home-content p, .about-content', { origin: 'right' });
